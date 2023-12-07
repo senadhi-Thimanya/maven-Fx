fe6c260685e956bbc10ac8772f8c5f470259dcca
senadhi-Thimanya <senadhithimanya777@gmail.com>
2023-12-07 16:24:35 +0530

    Created the fxml file for CustomerCorntroller Class

---
+++ src/main/java/Controller/CustomerFormController.java
@@ -0,0 +1,30 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CustomerFormController {
+
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    void btnOnActionSubmit(ActionEvent event) {
+
+    }
+
+}

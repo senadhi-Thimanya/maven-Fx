19f5af70a3839fecc70b4613228071f194e15e33
senadhi-Thimanya <senadhithimanya777@gmail.com>
2023-12-07 17:47:13 +0530

    Saving Customer in database

--- src/main/java/Controller/CustomerFormController.java
+++ src/main/java/Controller/CustomerFormController.java
@@ -1,9 +1,15 @@
 package Controller;
 
+import db.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class CustomerFormController {
 
@@ -24,6 +30,26 @@ public class CustomerFormController {
 
     @FXML
     void btnOnActionSubmit(ActionEvent event) {
+        String id = txtID.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        double salary = Double.parseDouble(txtSalary.getText());
+
+        String sql = "INSERT INTO Customer VALUES('"+id+"','"+name+"','"+address+"',"+salary+")";
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            Statement stm = connection.createStatement();
+            boolean isSaved = stm.executeUpdate(sql) > 0;
+            if (isSaved){
+                new Alert(Alert.AlertType.INFORMATION, "Saved Successfully!").show();
+            }else{
+                new Alert(Alert.AlertType.ERROR, "Something Went Wrong!").show();
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
